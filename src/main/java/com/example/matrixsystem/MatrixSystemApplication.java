7ea8bc8795e5c0c6d65b65a55430a4b2eba9e2d0
Supletsov Dmitriy <supletsovd@gmail.com>
2024-01-08 16:28:37 +0300

    'initialization'

---
+++ src/main/java/com/example/matrixsystem/MatrixSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.matrixsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatrixSystemApplication.class, args);
+    }
+
+}
