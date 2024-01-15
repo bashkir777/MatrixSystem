48b3fe263094ebe705501ecc0b5078bf87f07645
Supletsov Dmitriy <supletsovd@gmail.com>
2024-01-15 13:38:39 +0300

    Добавлено представление error-view, небольшой рефактор MainController, в task-template теперь отображается задание из БД, без дизайна

--- src/main/java/com/example/matrixsystem/controllers/MainController.java
+++ src/main/java/com/example/matrixsystem/controllers/MainController.java
@@ -1,14 +1,28 @@
 package com.example.matrixsystem.controllers;
 
+import com.example.matrixsystem.beans.DatabaseManager;
+import com.example.matrixsystem.spring_data.entities.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+
 
 @Controller
 @RequestMapping("/app")
 public class MainController {
+
+    private final DatabaseManager manager;
+    @Autowired
+    public MainController(DatabaseManager manager){
+        this.manager = manager;
+    }
 
     @GetMapping("/all-tasks")
     public String hello(){
@@ -16,9 +30,16 @@ public class MainController {
     }
 
     @GetMapping("/all-tasks/module/{moduleNum}/task/{taskNum}")
-    public String hello(@PathVariable Integer moduleNum,@PathVariable Integer taskNum, Model model) {
-        model.addAttribute("module", moduleNum);
-        model.addAttribute("task", taskNum);
-        return "task-template";
+    public ModelAndView hello(@PathVariable int moduleNum, @PathVariable int taskNum, Model model) {
+        ArrayList<Task> list = manager.getAllModuleTasks(moduleNum);
+        try{
+            Task task = list.get(taskNum-1);
+            model.addAttribute("taskNum", taskNum);
+            model.addAttribute("task", task);
+            return new ModelAndView("task-template", model.asMap());
+        }catch (IndexOutOfBoundsException | NullPointerException e){
+            model.addAttribute("error", "Задания с таким id нет в данном модуле");
+            return new ModelAndView("error-view", model.asMap());
+        }
     }
 }
