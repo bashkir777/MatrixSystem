8efb625ea6c40b9e04cc714840ce0ee87bb5d88f
Supletsov Dmitriy <supletsovd@gmail.com>
2024-02-10 16:26:51 +0300

    feature: добавлена шаблонизированная панель навигации в раздел теории

--- src/main/resources/static/javascript/closed-source/theory_logic.js
+++ src/main/resources/static/javascript/closed-source/theory_logic.js
@@ -1,5 +1,7 @@
 let sections = document.getElementsByClassName("section");
 let dropDowns = document.getElementsByClassName("drop-down");
+let navigationButtons = document.getElementsByClassName("navigation-button-wrapper");
+
 for(let i = 0; i < sections.length; i++){
     sections[i].addEventListener("click",
         ()=>{
@@ -22,4 +24,15 @@ tables.forEach(function(table) {
         })
     }
 });
+
+for (let button of navigationButtons){
+    button.addEventListener("click", () =>{
+        let task_id = 'task_' + button.id.split("_")[1];
+        let paragraph = document.getElementById(task_id);
+        let paragraphRect = paragraph.getBoundingClientRect();
+        let offsetTop = paragraphRect.top + window.scrollY;
+        let scrollOffset = offsetTop - window.innerHeight / 2 + paragraph.offsetHeight / 2;
+        window.scrollTo({ top: scrollOffset+200, left: 0, behavior: 'smooth' });
+    });
+}
 
