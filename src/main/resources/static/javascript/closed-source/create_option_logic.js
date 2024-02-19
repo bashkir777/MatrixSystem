ee336ce63295422bf5cfd20dffa857ace5efd02c
Supletsov Dmitriy <supletsovd@gmail.com>
2024-02-19 13:48:21 +0300

    feature: дизайн добавления заданий в вариант + фильтрация их по возрастанию номера задания

--- src/main/resources/static/javascript/closed-source/create_option_logic.js
+++ src/main/resources/static/javascript/closed-source/create_option_logic.js
@@ -2,6 +2,7 @@ let moduleNumInput = document.getElementById("module-num")
 let choose = document.getElementById("choose");
 let taskNum = document.getElementById("task-num");
 let taskText = document.getElementById("task-text");
+let scrollPanel = document.getElementById("scroll-panel");
 let currentTask = document.getElementById("current-task");
 let add = document.getElementById("add");
 let currentTaskId = null;
@@ -85,6 +86,54 @@ add.addEventListener("click", () => {
     for (let entry of optionToCreate) {
         if (entry.id === Number(currentModuleId)) {
             entry.taskId = currentTaskId;
+
+            for(let task of currentModuleTaskList){
+                if (task.id === currentTaskId){
+                    let taskProxy = document.createElement('div');
+                    taskProxy.id = "task_" + currentModuleId;
+                    taskProxy.classList.add("task-proxy");
+
+                    let moduleNum = document.createElement('div');
+                    moduleNum.classList.add("module-num");
+                    moduleNum.innerText = "Задание №"+ currentModuleId;
+                    taskProxy.appendChild(moduleNum);
+
+                    let text = document.createElement('div');
+                    text.classList.add("text");
+                    text.innerText = task.task;
+                    taskProxy.appendChild(text);
+
+                    let div = document.createElement('div');
+                    div.classList.add("text-align-right");
+                    let deleteButton = document.createElement('div');
+                    deleteButton.classList.add("delete");
+                    deleteButton.id = "delete_" + task.id;
+                    deleteButton.innerText = "Удалить";
+                    div.appendChild(deleteButton);
+
+                    deleteButton.addEventListener("click", ()=>{
+                        scrollPanel.removeChild(taskProxy);
+                        entry.taskId = null;
+                    });
+
+                    taskProxy.appendChild(div);
+
+                    scrollPanel.appendChild(taskProxy);
+
+                    let sortedChildren = Array.from(scrollPanel.children).sort((a, b) => {
+                        let idA = parseInt(a.getAttribute('id').split('_')[1]);
+                        let idB = parseInt(b.getAttribute('id').split('_')[1]);
+                        return idA - idB;
+                    });
+
+                    while (scrollPanel.firstChild){
+                        scrollPanel.removeChild(scrollPanel.firstChild);
+                    }
+                    for(let child of sortedChildren){
+                        scrollPanel.appendChild(child);
+                    }
+                }
+            }
             break;
         }
     }
