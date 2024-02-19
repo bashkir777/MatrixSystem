f2c9bbd6ba924b87eca231db4d48bae187977f2e
Supletsov Dmitriy <supletsovd@gmail.com>
2024-02-19 09:25:35 +0300

    + users one to many option

--- src/main/java/com/example/matrixsystem/spring_data/entities/Option.java
+++ src/main/java/com/example/matrixsystem/spring_data/entities/Option.java
@@ -1,9 +1,5 @@
 package com.example.matrixsystem.spring_data.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,10 +20,7 @@ public class Option {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-//    @ManyToOne
-//    @JoinColumn(name = "creator")
-//    @JsonBackReference
-//    private Users creator;
+    private Integer creator;
 
     @OneToMany(mappedBy = "option", fetch = FetchType.EAGER)
     private Set<OptionTask> optionTasks = new HashSet<>();
--- src/main/java/com/example/matrixsystem/spring_data/entities/Users.java
+++ src/main/java/com/example/matrixsystem/spring_data/entities/Users.java
@@ -1,10 +1,6 @@
 package com.example.matrixsystem.spring_data.entities;
 
 import com.example.matrixsystem.spring_data.entities.enums.Roles;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,10 +30,6 @@ public class Users {
 
     @OneToMany(mappedBy = "user")
     private Set<UserSection> userSections = new HashSet<>();
-
-//    @OneToMany(mappedBy = "creator")
-//    @JsonManagedReference
-//    private Set<Option> options = new HashSet<>();
 
     @OneToMany(mappedBy = "userReference", fetch = FetchType.EAGER)
     private Set<UserTask> userTask = new HashSet<>();
