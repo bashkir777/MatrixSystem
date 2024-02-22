5894c395e5a2413a4104333d602b721aad1feecb
Supletsov Dmitriy <supletsovd@gmail.com>
2024-02-22 20:03:31 +0300

    feature: отображение текста и номера задания в шаблоне ДЗ

--- src/main/resources/static/javascript/closed-source/homework_template_logic.js
+++ src/main/resources/static/javascript/closed-source/homework_template_logic.js
@@ -6,6 +6,13 @@ let arrowRight = document.getElementById("arrow_right");
 let scrollLeft = document.getElementById("scroll_left");
 let scrollRight = document.getElementById("scroll_right");
 let navigationButtonsWrapper = document.getElementById("navigation_buttons_wrapper");
+let taskNum = document.getElementById("task-num");
+let solution = document.getElementById("full-answer");
+let imageContainer = document.getElementById("image-container");
+let taskText = document.getElementById("task-text");
+let answerText = document.getElementById("_answer");
+let send = document.getElementById("send");
+let currentTaskId;
 let currentTaskOrder;
 function getModuleNumFromUrl() {
     return location.href.split("/").pop().split("?")[0];
@@ -37,6 +44,16 @@ fetch(`/api/v1/management/homework/${getModuleNumFromUrl()}`, {
                     b.classList.remove("navigation-tab-selected");
                 }
                 button.classList.add("navigation-tab-selected");
+                currentTaskId = data[i].id;
+                taskNum.innerText = currentTaskOrder;
+                taskText.innerText = data[i].task;
+                imageContainer.classList.add("display-none");
+                if (data[i].img !== null) {
+                    let imgSrc = data[i].img.replace(/\\/g, '/');
+                    imageContainer.src = location.protocol + "//" + location.host + "/" + imgSrc;
+                    imageContainer.classList.remove("display-none");
+                }
+
             })
         }
         navigationButtons[0].click();
