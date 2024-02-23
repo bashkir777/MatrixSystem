194f7c808d08a1e24592afbd22c27a80769bfa07
Supletsov Dmitriy <supletsovd@gmail.com>
2024-02-23 11:12:27 +0300

    bug-fix: bugfix hashcode

--- src/main/java/com/example/matrixsystem/spring_data/entities/UserSection.java
+++ src/main/java/com/example/matrixsystem/spring_data/entities/UserSection.java
@@ -24,4 +24,11 @@ public class UserSection {
     @ManyToOne
     @JoinColumn(name = "section_id")
     private Section section;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 }
--- src/main/java/com/example/matrixsystem/spring_data/entities/UserTask.java
+++ src/main/java/com/example/matrixsystem/spring_data/entities/UserTask.java
@@ -26,4 +26,11 @@ public class UserTask {
 
     @Enumerated(EnumType.STRING)
     private UserTaskRelationTypes relationType;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 }
