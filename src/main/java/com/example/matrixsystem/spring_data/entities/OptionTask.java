a5c66e8c9965a454aa1f102bf2eb247643de579f
Supletsov Dmitriy <supletsovd@gmail.com>
2024-02-25 21:24:44 +0300

    bugfix

--- src/main/java/com/example/matrixsystem/spring_data/entities/OptionTask.java
+++ src/main/java/com/example/matrixsystem/spring_data/entities/OptionTask.java
@@ -24,4 +24,11 @@ public class OptionTask {
     @ManyToOne
     @JoinColumn(name = "task_id")
     private Task task;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 }
